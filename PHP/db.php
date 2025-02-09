92710e287400e4614a3a75c170446a42f2b9bf5d
Samy Touabi <stoua026@uottawa.ca>
2025-02-09 18:03:06 -0500

    changed to more permanent database

--- PHP/db.php
+++ PHP/db.php
@@ -2,7 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$dbconn = pg_connect("postgresql://dental_clinic_db_jlct_user:u1Brn4dDKJR5qVvJcWPgWt1oRteiFKb6@dpg-cukgcodds78s739nrlj0-a.virginia-postgres.render.com/dental_clinic_db_jlct");
+// Render DB (doesnt last long)
+// $dbconn = pg_connect("postgresql://dental_clinic_db_jlct_user:u1Brn4dDKJR5qVvJcWPgWt1oRteiFKb6@dpg-cukgcodds78s739nrlj0-a.virginia-postgres.render.com/dental_clinic_db_jlct");
+
+$dbconn = pg_connect("postgresql://neondb_owner:npg_8yEIBgKxeVJ1@ep-frosty-glade-a4irks7k-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
+
 
 // SUPRESS WARNINGS
 // if (!$dbconn) {
