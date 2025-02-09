d255ccaccc24535b2d08f42ec1433ef1b461ef59
Samy Touabi <stoua026@uottawa.ca>
2025-02-09 14:54:51 -0500

    updated db link

--- PHP/db.php
+++ PHP/db.php
@@ -1,5 +1,4 @@
-<?php 
+<?php
 // Configure the postgres parameters here to your own settings when running locally
 // Do not overwrite this when pushing to git
-$dbconn=pg_connect(getenv("DATABASE_URL")); 
-?>
+$dbconn = pg_connect("postgresql://dental_clinic_db_jlct_user:u1Brn4dDKJR5qVvJcWPgWt1oRteiFKb6@dpg-cukgcodds78s739nrlj0-a.virginia-postgres.render.com/dental_clinic_db_jlct");
