3cd4ada831a477c3a86f088903c7078329c7fdd1
Samy Touabi <stoua026@uottawa.ca>
2025-02-09 16:09:23 -0500

    updated db connection

--- PHP/db.php
+++ PHP/db.php
@@ -1,4 +1,11 @@
 <?php
-// Configure the postgres parameters here to your own settings when running locally
-// Do not overwrite this when pushing to git
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $dbconn = pg_connect("postgresql://dental_clinic_db_jlct_user:u1Brn4dDKJR5qVvJcWPgWt1oRteiFKb6@dpg-cukgcodds78s739nrlj0-a.virginia-postgres.render.com/dental_clinic_db_jlct");
+
+if (!$dbconn) {
+    die("Database connection failed: " . pg_last_error());
+} else {
+    echo "Database connected successfully!";
+}
